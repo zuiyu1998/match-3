4b29eee91c66f3ac48d90e47f292be24994e2a97
zuiyu1998 <1542844298@qq.com>
2025-04-08 23:33:29 +0800

    feat: 设置棋盘

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,66 @@
 use bevy::prelude::*;
 
+pub const WIDTH: f32 = 288.0;
+pub const HEIGHT: f32 = 512.0;
+
 pub struct GamePlugin;
 
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, setup);
+        app.add_systems(Startup, setup_game);
     }
 }
 
-pub fn setup(mut commands: Commands) {
+fn setup_game(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(game_ui(&asset_server));
+}
+
+fn game_ui(asset_server: &AssetServer) -> impl Bundle + use<> {
+    let bg = asset_server.load("UI/Backgrounds/background 2.png");
+    let top = asset_server.load("UI/Top UI v 2.png");
+    let bottom = asset_server.load("UI/Bottom UI v 2.png");
+
+    (
+        Node {
+            width: Val::Percent(100.0),
+            height: Val::Percent(100.0),
+            ..default()
+        },
+        children![(
+            ImageNode::new(bg),
+            Node {
+                flex_direction: FlexDirection::Column,
+                align_items: AlignItems::Center,
+                justify_content: JustifyContent::SpaceBetween,
+                width: Val::Percent(100.0),
+                height: Val::Percent(100.0),
+
+                ..default()
+            },
+            children![
+                (
+                    ImageNode::new(top),
+                    Node {
+                        width: Val::Percent(100.0),
+                        height: Val::Px(96.0),
+                        ..default()
+                    },
+                ),
+                (
+                    ImageNode::new(bottom),
+                    Node {
+                        width: Val::Percent(100.0),
+                        height: Val::Px(46.0),
+                        ..default()
+                    },
+                )
+            ],
+        )],
+    )
+}
+
+fn setup(mut commands: Commands) {
     commands.spawn(Camera2d);
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
-use bevy::prelude::*;
-use match_3::GamePlugin;
+use bevy::{prelude::*, window::WindowResolution};
+use match_3::{GamePlugin, HEIGHT, WIDTH};
 
 fn main() {
     let mut app = App::new();
 
-    app.add_plugins((DefaultPlugins, GamePlugin));
+    app.add_plugins((
+        DefaultPlugins
+            .set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: "match_3".to_string(),
+                    resolution: WindowResolution::new(WIDTH, HEIGHT),
+                    ..default()
+                }),
+                ..default()
+            })
+            .set(ImagePlugin::default_nearest()),
+        GamePlugin,
+    ));
 
     app.run();
 }
